22fb30cfab121f66eb7f040a029c1bc8a3fd5d25
Rachel Hamby <tnthreat33@gmail.com>
2024-02-04 15:47:57 -0500

    practice

--- multiplier.ts
+++ multiplier.ts
@@ -1,4 +1,14 @@
-const multiplicator = (a: number, b: number, printText: string) => {
-    console.log(printText, a * b);
+type Opertion = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Opertion) =>{
+    if (op === 'multiply'){
+        return a * b;
+    } else if ( op === 'add'){
+        return a + b
+    } else if (op === 'divide'){
+        if (b === 0) return 'can\'t divide by 0!';
+        return a/b
+    }
 }
-multiplicator(2,4, 'Multiplied numbers 2 and 4, the result is:');
+
+console.log(calculator(1,2,'add'))
