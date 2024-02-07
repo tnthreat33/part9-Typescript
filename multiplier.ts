09b4eda524ea0c2d9b60e1213125b6063e429428
Rachel Hamby <tnthreat33@gmail.com>
2024-02-06 19:48:15 -0500

    practice

--- multiplier.ts
+++ multiplier.ts
@@ -2,4 +2,7 @@ const multiplicator = (a: number, b: number, printText: string) => {
     console.log(printText,  a * b);
   }
   
+  const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
   multiplicator(2, 4, 'Multiplied a string and 4, the result is:');
