f50d74a7079e72228fa30e2a94560c895f6d81a2
Rachel Hamby <tnthreat33@gmail.com>
2024-02-06 19:45:15 -0500

    practice

--- multiplier.ts
+++ multiplier.ts
@@ -1,28 +1,5 @@
-type Operation = 'multiply' | 'add' | 'divide';
-
-
-const calculator = (a: number, b: number, op: Operation) : number => {
-  switch(op) {
-    case 'multiply':
-      return a * b;
-    case 'divide':
-
-      if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    case 'add':
-      return a + b;
-    default:
-
-      throw new Error('Operation is not multiply, add or divide!');
+const multiplicator = (a: number, b: number, printText: string) => {
+    console.log(printText,  a * b);
   }
-}
-
-try {
-  console.log(calculator(1, 5 , 'divide'));
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
-  if (error instanceof Error) {
-    errorMessage += error.message;
-  }
-  console.log(errorMessage);
-}
+  
+  multiplicator(2, 4, 'Multiplied a string and 4, the result is:');
