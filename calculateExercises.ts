6777eb8582805ec8d3e4d6f4279c4ac424fc2407
Rachel Hamby <tnthreat33@gmail.com>
2024-02-19 12:04:33 -0500

    9.2 practice

---
+++ calculateExercises.ts
@@ -0,0 +1,9 @@
+interface Result {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
