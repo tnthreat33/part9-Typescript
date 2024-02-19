3823420f77e6bda1cee403b43e70ff5695f634d4
Rachel Hamby <tnthreat33@gmail.com>
2024-02-19 12:05:32 -0500

    9.2 practice

--- calculateExercises.ts
+++ calculateExercises.ts
@@ -7,3 +7,36 @@ interface Result {
     target: number;
     average: number;
 }
+
+const calculateExercises = (hours: number[], target: number): Result => {
+    const periodLength: number = hours.length;
+    const trainingDays: number = hours.filter(hour => hour > 0).length;
+    const sum: number = hours.reduce((a, b) => a + b, 0);
+    const average: number = sum / periodLength;
+    const success: boolean = average >= target;
+
+    let rating: number;
+    let ratingDescription: string;
+    if (average < target / 2) {
+        rating = 1;
+        ratingDescription = 'not so good, you should try harder';
+    } else if (average < target) {
+        rating = 2;
+        ratingDescription = 'not too bad but could be better';
+    } else {
+        rating = 3;
+        ratingDescription = 'excellent, you achieved your target';
+    }
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    };
+}
+
+export default calculateExercises;
